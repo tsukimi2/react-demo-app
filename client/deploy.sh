4175a26a5b33f569d5617b86ce8e5efb294fe045
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-05-29 16:57:03 +0530

    Moved deploy.sh to the client directory

--- client/deploy.sh
+++ client/deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a build/ travis@68.183.94.49:/home/abhiap/test/
+  rsync -a build/ travis@68.183.94.49:/home/abhiap/test
 else
   echo "Not deploying, since this branch isn't master."
 fi
