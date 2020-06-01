860527935a8cec9c5e807e26b621098ed1600f47
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-06-01 15:59:44 +0530

    Final push

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   rsync -a client/build/ travis@68.183.94.49:/home/abhiap/test/client
   rsync -a server/ travis@68.183.94.49:/home/abhiap/test/server
 else
-  echo "Not deploying, since this branch isn't master."
+  echo "Not deploying, since the branch isn't master."
 fi
