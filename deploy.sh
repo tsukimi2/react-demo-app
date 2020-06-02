e2417abaac259a89d5754e958e76a72798f18e1e
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-06-02 14:20:50 +0530

    Removed hardcodings from deploy.sh and .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# Change <ipaddress> to your server's ip
+# Replace <path> with the appropriate paths
+
 set -xe
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@68.183.94.49:/home/abhiap/test/client
-  rsync -a server/ travis@68.183.94.49:/home/abhiap/test/server
+  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@<ip address>:<path>
+  rsync -a server/ travis@<ip address>:<path>
 
 else
   echo "Not deploying, since the branch isn't master."
 fi
 
-ssh travis@68.183.94.49 'pm2 restart all'
+ssh travis@<ip address> 'pm2 restart all'
