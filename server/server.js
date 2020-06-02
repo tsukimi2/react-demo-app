5b38ef37d6de2bff15a9d13935bd55665f5ce68d
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-06-02 14:02:04 +0530

    Changed port number in server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
