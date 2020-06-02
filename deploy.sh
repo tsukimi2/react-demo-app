085d432178035a89e688ad76ce7a7c19d231b042
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-06-02 11:50:46 +0530

    More changes to the client rsync command

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --include 'build' --include '*.json' client/ travis@68.183.94.49:/home/abhiap/test/client
-  rsync -a server/ travis@68.183.94.49:/home/abhiap/test/serve
+  rsync -a --exclude={'node_modules', 'src', 'public'} client/ travis@68.183.94.49:/home/abhiap/test/client
+  rsync -a server/ travis@68.183.94.49:/home/abhiap/test/server
 
 else
   echo "Not deploying, since the branch isn't master."
