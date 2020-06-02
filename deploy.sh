611e880d402e08dd5eb2ea0b9fa3753ad1294cba
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-06-02 12:30:31 +0530

    Changed to deploy user in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ abhiap@68.183.94.49:/home/abhiap/test/client
+  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@68.183.94.49:/home/abhiap/test/client
   rsync -a server/ travis@68.183.94.49:/home/abhiap/test/server
 
 else
