019eb5903617caee56386d98103df19f23f620c7
tsukimi2 <shigotoganbatte4@gmail.com>
2021-09-19 19:54:12 -0700

    testing

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@<ip address>:<path>
-  rsync -a server/ travis@<ip address>:<path>
+  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@167.172.82.94:/home/osiris/demo/client
+  rsync -a server/ travis@167.172.82.94:/home/osiris/demo/server
 
 else
   echo "Not deploying, since the branch isn't master."
